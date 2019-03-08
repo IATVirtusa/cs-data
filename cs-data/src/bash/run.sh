d065f88d6edef03328ed1b561aa8f6e46a4170ac
prabhu <pshankar@virtusa.com>
2019-03-08 11:35:34 -0500

    kk

--- cs-data/src/bash/run.sh
+++ cs-data/src/bash/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # create new table if does not exists
-
+echo"current PWD is
 echo `pwd`
-hive -f ../conf/cars.sql
+my_dir=`dirname $0`
+cat ../$my_dir/conf/cars.sql
+#hive -f ../$my_dir/conf/cars.sql
 
 # query the table, you can collect the output of it into some file
-hive -e "select * from test.cars"
+#hive -e "select * from test.cars"
