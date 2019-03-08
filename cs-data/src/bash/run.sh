2ebe9d7b7ba0af4c3cdcc766cdebdcb90bdb7669
prabhu <pshankar@virtusa.com>
2019-03-08 11:56:27 -0500

    ff

--- cs-data/src/bash/run.sh
+++ cs-data/src/bash/run.sh
@@ -5,9 +5,9 @@ echo "current PWD is"
 echo `pwd`
 my_dir=`dirname $0`
 
-cat ../$my_dir/conf/cars.sql
+cat $my_dir/../conf/cars.sql
 
-hive -f ../$my_dir/conf/cars.sql
+hive -f $my_dir/../conf/cars.sql
 
 # query the table, you can collect the output of it into some file
 hive -e "select * from test.cars" >> /var/lib/jenkins/test.txt
