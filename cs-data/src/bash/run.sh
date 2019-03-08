d9f39f7e474aa1b5c42b473188eb5ffdd4f38284
prabhu <pshankar@virtusa.com>
2019-03-08 11:44:03 -0500

    ll

--- cs-data/src/bash/run.sh
+++ cs-data/src/bash/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # create new table if does not exists
-echo"current PWD is
+echo "current PWD is"
 echo `pwd`
 my_dir=`dirname $0`
+
 cat ../$my_dir/conf/cars.sql
-#hive -f ../$my_dir/conf/cars.sql
+
+hive -f ../$my_dir/conf/cars.sql
 
 # query the table, you can collect the output of it into some file
-#hive -e "select * from test.cars"
+hive -e "select * from test.cars" >> /var/lib/jenkins/test.txt
